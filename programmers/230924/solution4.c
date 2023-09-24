eec94c6e3ddf33acf9cb846f2eba30c4706c1057
WeepingHeron <kimtaehoon_98@naver.com>
2023-09-24 21:59:46 +0900

    순서쌍의 개수

---
+++ programmers/230924/solution4.c
@@ -0,0 +1,18 @@
+// 순서쌍의 개수
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n)
+{
+    int i;
+    int answer = 0;
+    
+    for (i = 1; i <= n; i++)
+    {
+        if (n % i == 0)
+            answer++;
+    }
+    return answer;
+}
